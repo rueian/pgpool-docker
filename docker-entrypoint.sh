5d52969132ed9c26f5696c3c4d05ac39af98b6eb
Rueian Oneecom <rueiancsie@gmail.com>
2018-10-22 16:48:47 +0800

    override pool and connection parameters from env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 for filename in pgpool.conf pool_hba.conf pool_passwd
 do
   [ -f /etc/pgpool/${filename} ] && cp /etc/pgpool/${filename} /etc/pgpool2/${filename}
+done
+
+for param in num_init_children max_pool child_life_time child_max_connections connection_life_time client_idle_limit
+do
+  envvar=$(echo ${param} | tr a-z A-Z)
+  [ ! -z "${!envvar}" ] && sed -i 's/'$param' =.*/'$param' = '${!envvar}'/g' /etc/pgpool2/pgpool.conf
 done
 
 exec "$@"
